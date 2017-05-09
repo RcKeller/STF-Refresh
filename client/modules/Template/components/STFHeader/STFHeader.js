4d3cf0fc5a0984e1f99309c242032c28ea20e1f3
Ryan Keller <rykeller@uw.edu>
2017-05-09 11:36:31 -0700

    Going smoothlygit add .!

--- client/modules/Template/components/STFHeader/STFHeader.js
+++ client/modules/Template/components/STFHeader/STFHeader.js
@@ -1,10 +1,39 @@
 import React, { PropTypes } from 'react'
 
+import Nav from 'react-bootstrap/lib/Nav'
+import Navbar from 'react-bootstrap/lib/Navbar'
+import NavItem from 'react-bootstrap/lib/NavItem'
+import NavDropdown from 'react-bootstrap/lib/NavDropdown'
+import MenuItem from 'react-bootstrap/lib/MenuItem'
+
 // Import Style
-// import styles from '../uw.css'
+import styles from './STFHeader.css'
+// Import Images
+import STFLogo from './stf.png'
 
 export function STFHeader (props, context) {
-  return (<div>STF Header</div>
+  return (
+    <Navbar>
+      <Navbar.Header>
+        <Navbar.Brand className={styles['navbar-brand']}>
+          <img src={STFLogo}/>
+        </Navbar.Brand>
+        <Navbar.Toggle />
+      </Navbar.Header>
+      <Navbar.Collapse>
+      <Nav>
+        <NavItem eventKey={1} href='#'>Link</NavItem>
+        <NavItem eventKey={2} href='#'>Link</NavItem>
+        <NavDropdown eventKey={3} title='Dropdown' id='basic-nav-dropdown'>
+          <MenuItem eventKey={3.1}>Action</MenuItem>
+          <MenuItem eventKey={3.2}>Another action</MenuItem>
+          <MenuItem eventKey={3.3}>Something else here</MenuItem>
+          <MenuItem divider />
+          <MenuItem eventKey={3.4}>Separated link</MenuItem>
+        </NavDropdown>
+      </Nav>
+    </Navbar.Collapse>
+    </Navbar>
   )
 }
 STFHeader.contextTypes = {
--- server/server.js
+++ server/server.js
@@ -80,6 +80,7 @@ const renderFullPage = (html, initialState) => {
         ${process.env.NODE_ENV === 'production' ? `<link rel='stylesheet' href='${assetsManifest['/app.css']}' />` : ''}
         <link href='https://fonts.googleapis.com/css?family=Lato:400,300,700' rel='stylesheet' type='text/css'/>
         <link rel="shortcut icon" href="http://res.cloudinary.com/hashnode/image/upload/v1455629445/static_imgs/mern/mern-favicon-circle-fill.png" type="image/png" />
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/latest/css/bootstrap.min.css">
       </head>
       <body>
         <div id="root">${html}</div>
