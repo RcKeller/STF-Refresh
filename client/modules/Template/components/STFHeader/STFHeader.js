c20505ac2ef04b813ea9da95d6df2efc6d81dbd4
Ryan Keller <rykeller@uw.edu>
2017-05-09 16:19:58 -0700

    Further integration. Images are a tad hacky due to bsStyles.

--- client/modules/Template/components/STFHeader/STFHeader.js
+++ client/modules/Template/components/STFHeader/STFHeader.js
@@ -1,50 +1,65 @@
-import React, { PropTypes } from 'react'
+import React from 'react'
 import { Link } from 'react-router'
+import { LinkContainer } from 'react-router-bootstrap'
 
 import Nav from 'react-bootstrap/lib/Nav'
 import Navbar from 'react-bootstrap/lib/Navbar'
 import NavItem from 'react-bootstrap/lib/NavItem'
 import NavDropdown from 'react-bootstrap/lib/NavDropdown'
 import MenuItem from 'react-bootstrap/lib/MenuItem'
-
+import Image from 'react-bootstrap/lib/Image'
 // Import Style
 import styles from './STFHeader.css'
 // Import Images
-import STFLogo from './stf.png'
+// import STFLogo from './stf.png'
 
 export function STFHeader (props, context) {
   return (
     <Navbar>
       <Navbar.Header>
-        <Link to='/' >
-        {/* <Navbar.Link href='/'> */}
-          <Navbar.Brand className={styles['navbar-brand']}>
-            <img src={STFLogo} />
+        <Link to='/'>
+          {/* <Navbar.Brand className={styles['navbar-image']}> */}
+          {/* <img src={STFLogo} /> */}
+          <Navbar.Brand>
+            <div className={styles['stf-logo']} />
           </Navbar.Brand>
-        {/* </Navbar.Link> */}
         </Link>
         <Navbar.Toggle />
+
       </Navbar.Header>
       <Navbar.Collapse>
         <Nav>
           <NavDropdown eventKey={3} title='Proposals' id='basic-nav-dropdown'>
-            <MenuItem eventKey={3.1}>Browse All</MenuItem>
-            <MenuItem eventKey={3.2}>My Proposals</MenuItem>
+            <LinkContainer to='/proposals/browse/' >
+              <MenuItem eventKey={3.1}>Browse All</MenuItem>
+            </LinkContainer>
+            <LinkContainer to='/proposals/edit/' >
+              <MenuItem eventKey={3.2}>My Proposals</MenuItem>
+            </LinkContainer>
             <MenuItem divider />
-            <MenuItem eventKey={3.3}>Submit a Proposal</MenuItem>
+            <LinkContainer to='/proposals/new/' >
+              <MenuItem eventKey={3.3}>Submit a Proposal</MenuItem>
+            </LinkContainer>
           </NavDropdown>
           <NavItem eventKey={1} href='#'>Blocks</NavItem>
           <NavItem eventKey={2} href='#'>Calendar</NavItem>
           <NavDropdown eventKey={3} title='Documents' id='basic-nav-dropdown'>
-            <MenuItem eventKey={3.1}>Public Documents</MenuItem>
-            <MenuItem eventKey={3.2}>License Keyserver</MenuItem>
-            <MenuItem eventKey={3.3}>Request for Proposals</MenuItem>
+            <LinkContainer to='/documents/' >
+              <MenuItem eventKey={3.1}>Public Documents</MenuItem>
+            </LinkContainer>
+            <MenuItem href='http://itconnect.uw.edu/wares/acquiring-software-and-hardware/keyserver-help-for-it-staff/' eventKey={3.2}>License Keyserver</MenuItem>
+            <LinkContainer to='/docs/rfp.pdf' >
+              <MenuItem eventKey={3.3}>Request for Proposals</MenuItem>
+            </LinkContainer>
           </NavDropdown>
-          {/* <Link to='#'> */}
-            <MenuItem pullRight>
-              NetID
-          </MenuItem>
-          {/* </Link> */}
+          <NavItem>
+            <Image className={styles['netid-avatar']} src='http://placehold.it/100x100' circle />
+          </NavItem>
+          <LinkContainer to='/shib/'>
+            <NavItem pullRight>
+                 NetID
+            </NavItem>
+          </LinkContainer>
         </Nav>
       </Navbar.Collapse>
     </Navbar>
--- server/server.js
+++ server/server.js
@@ -78,8 +78,8 @@ const renderFullPage = (html, initialState) => {
         ${head.script.toString()}
 
         ${process.env.NODE_ENV === 'production' ? `<link rel='stylesheet' href='${assetsManifest['/app.css']}' />` : ''}
+        ${process.env.NODE_ENV === 'production' ? `<link rel='shortcut icon' href='${assetsManifest['/favicon.ico']}' type="image/png" />` : '<link rel="shortcut icon" href="http://res.cloudinary.com/hashnode/image/upload/v1455629445/static_imgs/mern/mern-favicon-circle-fill.png" type="image/png" />'}
         <link href='https://fonts.googleapis.com/css?family=Lato:400,300,700' rel='stylesheet' type='text/css'/>
-        <link rel="shortcut icon" href="http://res.cloudinary.com/hashnode/image/upload/v1455629445/static_imgs/mern/mern-favicon-circle-fill.png" type="image/png" />
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/latest/css/bootstrap.min.css">
       </head>
       <body>
