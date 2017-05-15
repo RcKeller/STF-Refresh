b55d6edb0a2d50f9b2555fb3c44afe3f5bf4761b
Ryan Keller <rykeller@uw.edu>
2017-05-15 10:50:14 -0700

    mapping openkeys is a pain.

--- client/modules/Template/Template.js
+++ client/modules/Template/Template.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import { connect } from 'react-redux'
+import { browserHistory } from 'react-router'
 
 // Import Meta Components
 import DevTools from '../../DevTools'
@@ -23,9 +24,15 @@ import styles from './Template.css'
 export class Template extends React.Component {
   constructor (props) {
     super(props)
-    this.state = { mounted: false }
+    this.state = { mounted: false, selected: '/' }
+    this.highlightMenuItem = this.highlightMenuItem.bind(this)
   }
-  componentDidMount () { this.setState({mounted: true}) }
+  componentDidMount () {
+    this.setState({ mounted: true })
+    this.highlightMenuItem()
+  }
+  componentWillReceiveProps () { this.highlightMenuItem() }
+  highlightMenuItem () { this.setState({selected: window.location.pathname}) }
   render () {
     return (
       <Layout>
@@ -37,34 +44,41 @@ export class Template extends React.Component {
         <UWHeader />
         <Layout style={{ height: 'calc(100vh - 58px)' }}>
           <Sider breakpoint='md' width={240} collapsedWidth='0' style={{zIndex: 999, background: '#FFF'}}>
-            <div className={styles['logo']} />
-            <Menu mode='inline'>
+            <div className={styles['logo']} onClick={() => browserHistory.push('/')} />
+            <Menu mode='inline'
+              defaultSelectedKeys={['1']}
+              selectedKeys={[this.state.selected]}
+              onClick={(i) => i.key && browserHistory.push(i.key)}
+            >
               <SubMenu key='sub1' title={<span><Icon type='solution' /><span>Proposals</span></span>}>
                 <MenuItemGroup key='g1' title='Browse'>
-                  <Menu.Item key='1'>All Proposals</Menu.Item>
-                  <Menu.Item key='2'>My Proposals</Menu.Item>
-                  <Menu.Item key='3'>Funding Blocks</Menu.Item>
+                  <Menu.Item key='/proposals/browse'>All Proposals</Menu.Item>
+                  <Menu.Item key='/proposals/myproposals'>My Proposals</Menu.Item>
+                  <Menu.Item key='/blocks'>Funding Blocks</Menu.Item>
                 </MenuItemGroup>
                 <MenuItemGroup key='g2' title='Submit'>
-                  <Menu.Item key='4'>Proposal</Menu.Item>
-                  <Menu.Item key='5'>Block</Menu.Item>
+                  <Menu.Item key='/proposals/splash'>Proposal</Menu.Item>
                 </MenuItemGroup>
               </SubMenu>
               <SubMenu key='sub2' title={<span><Icon type='file' /><span>Documents</span></span>}>
-                <Menu.Item key='6'>Commitee Docs</Menu.Item>
-                <Menu.Item key='7'>Request for Proposals</Menu.Item>
-                <Menu.Item key='8'>License Keyserver</Menu.Item>
+                <Menu.Item key='/documents'>Commitee Docs</Menu.Item>
+                <Menu.Item key='/docs/Current Request for Proposals.pdf' >Request for Proposals</Menu.Item>
+                <Menu.Item key=''>
+                  <a href='http://itconnect.uw.edu/wares/acquiring-software-and-hardware/keyserver-help-for-it-staff/' target='_blank'>
+                    License Keyserver
+                  </a>
+                </Menu.Item>
               </SubMenu>
               <SubMenu key='sub3' title={<span><Icon type='team' /><span>About</span></span>}>
-                <Menu.Item key='9'>The Committee</Menu.Item>
-                <Menu.Item key='10'>Contact Us</Menu.Item>
+                <Menu.Item key='/about'>The Committee</Menu.Item>
+                <Menu.Item key='/contact'>Contact Us</Menu.Item>
               </SubMenu>
-              <Menu.Item key='11'>
+              <Menu.Item key='/faq'>
                 <Icon type='question' /><span className='nav-text'>FAQ</span>
               </Menu.Item>
               <SubMenu key='sub4' title={<span><Icon type='calendar' /><span>Calendar</span></span>}>
-                <Menu.Item key='12'>Schedule</Menu.Item>
-                <Menu.Item key='13'>Upcoming Events</Menu.Item>
+                <Menu.Item key='/calendar'>Schedule</Menu.Item>
+                <Menu.Item key='/posts'>Upcoming Events</Menu.Item>
               </SubMenu>
             </Menu>
           </Sider>
@@ -78,5 +92,6 @@ export class Template extends React.Component {
 }
 
 Template.propTypes = { children: PropTypes.object.isRequired }
+Template.contextTypes = { router: React.PropTypes.object }
 function mapStateToProps (store) { return {} }
 export default connect(mapStateToProps)(Template)
