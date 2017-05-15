17c6151a27c1d5f29f9b4c6d908d5a7e8a9ea952
Ryan Keller <rykeller@uw.edu>
2017-05-15 11:59:39 -0700

    Port of slick has issues apparently, skipping.

--- client/modules/FrontPage/FrontPage.js
+++ client/modules/FrontPage/FrontPage.js
@@ -5,14 +5,41 @@ import React from 'react'
 import { Carousel } from 'antd'
 // import Jumbotron from '../../components/Jumbotron/Jumbotron'
 
-// import styles from './FrontPage.css'
+const settings = {
+  className: 'center',
+  centerMode: true,
+  // centerPadding: 'px',
+  dots: true,
+  arrows: true,
+  infinite: true,
+  speed: 500,
+  slidesToShow: 1,
+  slidesToScroll: 1
+}
+
+const images = [
+  'https://photos.smugmug.com/Classroom/i-h6rZ3NP/0/469a2c2c/4K/Campus_August_2016-1576-4K.jpg',
+  'https://photos.smugmug.com/Classroom/i-K9xnSdc/0/6f3bc353/4K/_DSC2708-4K.jpg',
+  'https://photos.smugmug.com/Classroom/i-TD5DnHh/0/1b4c75e6/4K/Campus_72015-2001-4K.jpg'
+]
+
+import styles from './FrontPage.css'
 const FrontPage = () => (
   <article>
-    <Carousel vertical='true' autoplay>
-      <div><h3>1</h3></div>
+    <Carousel className={styles['carousel']} autoplay>
+      <div>
+        <img src={images[0]} />
+      </div>
+      <div>
+        <img src={images[1]} />
+      </div>
+      <div>
+        <img src={images[2]} />
+      </div>
+      {/* <div><h3>1</h3></div>
       <div><h3>2</h3></div>
       <div><h3>3</h3></div>
-      <div><h3>4</h3></div>
+      <div><h3>4</h3></div> */}
     </Carousel>
   </article>
 )
