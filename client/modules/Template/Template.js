fcbfab9196cdfc93915f38529f25062f58d5939e
Ryan Keller <rykeller@uw.edu>
2017-05-15 09:28:31 -0700

    About to integrate react-router.

--- client/modules/Template/Template.js
+++ client/modules/Template/Template.js
@@ -2,26 +2,24 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import { connect } from 'react-redux'
 
+// Import Meta Components
 import DevTools from '../../DevTools'
-
-// Import Components
 import Helmet from 'react-helmet'
-
-import { Layout, Menu, Icon } from 'antd'
-const { Header, Content, Footer, Sider } = Layout
-// const SubMenu = Menu.SubMenu;
-
-// import Nav from './components/Nav/Nav'
-import UWHeader from './components/UWHeader/UWHeader'
-// import STFHeader from './components/STFHeader/STFHeader'
-// import Footer from './components/Footer/Footer'
-
-import styles from './Template.css'
 const meta = [
   { charset: 'utf-8' },
   { 'http-equiv': 'X-UA-Compatible', content: 'IE=edge' },
   { name: 'viewport', content: 'width=device-width, initial-scale=1' }
 ]
+
+// UI Components
+import { Layout, Menu, Icon } from 'antd'
+const { Content, Sider } = Layout
+const SubMenu = Menu.SubMenu
+const MenuItemGroup = Menu.ItemGroup
+
+import UWHeader from './components/UWHeader/UWHeader'
+
+import styles from './Template.css'
 export class Template extends React.Component {
   constructor (props) {
     super(props)
@@ -41,38 +39,43 @@ export class Template extends React.Component {
           <Sider breakpoint='md' width={240} collapsedWidth='0' style={{zIndex: 999, background: '#FFF'}}>
             <div className={styles['logo']} />
             <Menu mode='inline'>
-              <Menu.Item key='1'>
-                <Icon type='solution' /><span className='nav-text'>Proposals</span>
+              <SubMenu key='sub1' title={<span><Icon type='solution' /><span>Proposals</span></span>}>
+                <MenuItemGroup key='g1' title='Browse'>
+                  <Menu.Item key='1'>All Proposals</Menu.Item>
+                  <Menu.Item key='2'>My Proposals</Menu.Item>
+                  <Menu.Item key='3'>Funding Blocks</Menu.Item>
+                </MenuItemGroup>
+                <MenuItemGroup key='g2' title='Submit'>
+                  <Menu.Item key='4'>Proposal</Menu.Item>
+                  <Menu.Item key='5'>Block</Menu.Item>
+                </MenuItemGroup>
+              </SubMenu>
+              <SubMenu key='sub2' title={<span><Icon type='file' /><span>Documents</span></span>}>
+                <Menu.Item key='6'>Commitee Docs</Menu.Item>
+                <Menu.Item key='7'>Request for Proposals</Menu.Item>
+                <Menu.Item key='8'>License Keyserver</Menu.Item>
+              </SubMenu>
+              <SubMenu key='sub3' title={<span><Icon type='team' /><span>About</span></span>}>
+                <Menu.Item key='9'>The Committee</Menu.Item>
+                <Menu.Item key='10'>Contact Us</Menu.Item>
+              </SubMenu>
+              <Menu.Item key='11'>
+                <Icon type='question' /><span className='nav-text'>FAQ</span>
               </Menu.Item>
-              <Menu.Item key='2'>
-                <Icon type='file' /><span className='nav-text'>Documents</span>
-              </Menu.Item>
-              <Menu.Item key='3'>
-                <Icon type='team' /><span className='nav-text'>About</span>
-              </Menu.Item>
-              <Menu.Item key='4'>
-                <Icon type='calendar' /><span className='nav-text'>Calendar</span>
-              </Menu.Item>
+              <SubMenu key='sub4' title={<span><Icon type='calendar' /><span>Calendar</span></span>}>
+                <Menu.Item key='12'>Schedule</Menu.Item>
+                <Menu.Item key='13'>Upcoming Events</Menu.Item>
+              </SubMenu>
             </Menu>
           </Sider>
           <Content>
             {this.props.children}
-            {/* <Footer style={{ textAlign: 'center' }}>Ant Design ©2016 Created by Ant UED</Footer> */}
           </Content>
         </Layout>
       </Layout>
     )
   }
 }
-/*
-// Import Actions
-import { toggleAddPost } from './TemplateActions'
-
-<a className={styles['add-post-button']} href='#' onClick={() => this.props.dispatch(toggleAddPost())}>
-  Debug Placeholder: Add a post!
-</a>
-// dispatch: PropTypes.func.isRequired
-*/
 
 Template.propTypes = { children: PropTypes.object.isRequired }
 function mapStateToProps (store) { return {} }
--- client/modules/Template/components/UWHeader/UWHeader.js
+++ client/modules/Template/components/UWHeader/UWHeader.js
@@ -2,7 +2,7 @@ import React from 'react'
 import PropTypes from 'prop-types'
 
 // Import Style
-import styles from '../uw.css'
+import styles from './uw.css'
 
 export function UWHeader () {
   return (
