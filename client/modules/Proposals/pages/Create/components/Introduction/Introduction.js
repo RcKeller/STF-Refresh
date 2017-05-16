f3dc7cd8cca5edd861be3b7e83de41988947f396
Ryan Keller <rykeller@uw.edu>
2017-05-16 16:43:54 -0700

    Scaffolding forms...

--- client/modules/Proposals/pages/Create/components/Introduction/Introduction.js
+++ client/modules/Proposals/pages/Create/components/Introduction/Introduction.js
@@ -2,21 +2,73 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import { connect } from 'react-redux'
 
-class Introduction extends React.Component {
-  // constructor (props) {
-  //   super(props)
-  //   this.state = { }
-  // }
-  render () {
+import { Form, Icon, Input, Button } from 'antd';
+const FormItem = Form.Item;
+
+function hasErrors(fieldsError) {
+  return Object.keys(fieldsError).some(field => fieldsError[field]);
+}
+
+class IntroductionForm extends React.Component {
+componentDidMount() {
+    // To disabled submit button at the beginning.
+    this.props.form.validateFields();
+  }
+  handleSubmit = (e) => {
+    e.preventDefault();
+    this.props.form.validateFields((err, values) => {
+      if (!err) {
+        console.log('Received values of form: ', values);
+      }
+    });
+  }
+  render() {
+    const { getFieldDecorator, getFieldsError, getFieldError, isFieldTouched } = this.props.form;
+
+    // Only show error after a field is touched.
+    const userNameError = isFieldTouched('userName') && getFieldError('userName');
+    const passwordError = isFieldTouched('password') && getFieldError('password');
     return (
-      <div>
-        Intro
-      </div>
+      <Form layout="inline" onSubmit={this.handleSubmit}>
+        <FormItem
+          validateStatus={userNameError ? 'error' : ''}
+          help={userNameError || ''}
+        >
+          {getFieldDecorator('userName', {
+            rules: [{ required: true, message: 'Please input your username!' }],
+          })(
+            <Input prefix={<Icon type="user" style={{ fontSize: 13 }} />} placeholder="Username" />
+          )}
+        </FormItem>
+        <FormItem
+          validateStatus={passwordError ? 'error' : ''}
+          help={passwordError || ''}
+        >
+          {getFieldDecorator('password', {
+            rules: [{ required: true, message: 'Please input your Password!' }],
+          })(
+            <Input prefix={<Icon type="lock" style={{ fontSize: 13 }} />} type="password" placeholder="Password" />
+          )}
+        </FormItem>
+        <FormItem>
+          <Button
+            type="primary"
+            htmlType="submit"
+            disabled={hasErrors(getFieldsError())}
+          >
+            Log in
+          </Button>
+        </FormItem>
+      </Form>
     )
   }
 }
 
-const mapStateToProps = (state) => { return {} }
-const mapDispatchToProps = (dispatch) => { return {} }
-// Introduction.propTypes = {}
-export default connect(mapStateToProps, mapDispatchToProps)(Introduction)
+const Introduction = Form.create()(IntroductionForm);
+export default Introduction
+
+
+// const mapStateToProps = (state) => { return {} }
+// const mapDispatchToProps = (dispatch) => { return {} }
+// // Introduction.propTypes = {}
+// export default connect(mapStateToProps, mapDispatchToProps)(Introduction)
