fc92833cd37d96efffb5698fea223ed6fbdbc73f
Ryan Keller <rykeller@uw.edu>
2017-05-18 15:15:02 -0700

    WOW... I think... I need to create my own NPM lib for my antd wrappers. Backing out 5 directories is a wee bit much.

---
+++ client/components/Form/Form.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+import { Form, Input as AntInput, Tooltip, Icon, Cascader, Select, Row, Col, Checkbox, Button, AutoComplete } from 'antd'
+const FormItem = Form.Item
+const Option = Select.Option
+const AutoCompleteOption = AutoComplete.Option
+
+export const Input = ({input, label}) => (
+  <AntInput
+    placeholder={label}
+    value={input.value}
+    onChange={input.onChange}
+  />
+)
--- client/modules/Proposals/pages/Create/components/Introduction/Introduction.js
+++ client/modules/Proposals/pages/Create/components/Introduction/Introduction.js
@@ -3,25 +3,27 @@ import PropTypes from 'prop-types'
 import { connect } from 'react-redux'
 import {Field, reduxForm} from 'redux-form'
 
-import { Form, Input, Tooltip, Icon, Cascader, Select, Row, Col, Checkbox, Button, AutoComplete } from 'antd'
-const FormItem = Form.Item
-const Option = Select.Option
-const AutoCompleteOption = AutoComplete.Option
+// import { Form, Input, Tooltip, Icon, Cascader, Select, Row, Col, Checkbox, Button, AutoComplete } from 'antd'
+// const FormItem = Form.Item
+// const Option = Select.Option
+// const AutoCompleteOption = AutoComplete.Option
+//
+// const renderInput = ({input, label}) => (
+//   <Input
+//     placeholder={label}
+//     value={input.value}
+//     onChange={input.onChange}
+//   />
+// )
 
-const renderInput = ({input, label}) => (
-  <Input
-    placeholder={label}
-    value={input.value}
-    onChange={input.onChange}
-  />
-)
+import { Input } from '../../../../../../components/Form/Form'
 
 const Introduction = props => {
   const {handleSubmit, pristine, reset, submitting} = props
   return (
     <form onSubmit={handleSubmit}>
       <Field name='name' label='Name'
-        component={renderInput}
+        component={Input}
         />
     </form>
   )
