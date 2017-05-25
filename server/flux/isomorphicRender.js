648f44b14621d2682ecc943bd7ff2d306e366753
Ryan Keller <rykeller@uw.edu>
2017-05-25 08:05:42 -0700

    Abstracted out isomorphic render. Don't define tags as const, though, it fails.

--- server/flux/isomorphicRender.js
+++ server/flux/isomorphicRender.js
@@ -2,15 +2,13 @@ import React from 'react'
 import Helmet from 'react-helmet'
 
 import serverConfig from '../config'
-
-//Bundles to include in page rendering.
-
-// Render Initial HTML
+// Render Initial HTML containing metadata and a <script> to set initial page state.
 export const renderPage = (html, initialState) => {
   const head = Helmet.rewind()
   // Import Manifests
   const assetsManifest = process.env.webpackAssets && JSON.parse(process.env.webpackAssets)
   const chunkManifest = process.env.webpackChunkAssets && JSON.parse(process.env.webpackChunkAssets)
+  //  This code is very sensitive. Don't refactor it, defining these tags outside of runtime causes them to fail.
   return `
     <!doctype html>
     <html>
@@ -20,20 +18,27 @@ export const renderPage = (html, initialState) => {
         ${head.meta.toString()}
         ${head.link.toString()}
         ${head.script.toString()}
-        ${serverConfig.env === 'production' ? `<link rel='stylesheet' href='${assetsManifest['/app.css']}' />` : ''}
-        ${serverConfig.env === 'production' ? `<link rel='shortcut icon' href='${assetsManifest['/favicon.ico']}' type="image/png" />` : '<link rel="shortcut icon" href="http://res.cloudinary.com/hashnode/image/upload/v1455629445/static_imgs/mern/mern-favicon-circle-fill.png" type="image/png" />'}
+        ${serverConfig.env === 'production'
+          ? `<link rel='stylesheet' href='${assetsManifest['/app.css']}' />` : ''}
+        ${serverConfig.env === 'production'
+          ? `<link rel='shortcut icon' href='${assetsManifest['/favicon.ico']}' type="image/png" />`
+          : '<link rel="shortcut icon" href="http://res.cloudinary.com/hashnode/image/upload/v1455629445/static_imgs/mern/mern-favicon-circle-fill.png" type="image/png" />'}
       </head>
       <body>
         <div id="root">${html}</div>
         <script>
           window.__INITIAL_STATE__ = ${JSON.stringify(initialState)};
           ${serverConfig.env === 'production'
-          ? `//<![CDATA[
-          window.webpackManifest = ${JSON.stringify(chunkManifest)};
-          //]]>` : ''}
+            ? `//<![CDATA[
+            window.webpackManifest = ${JSON.stringify(chunkManifest)};
+            //]]>` : ''}
         </script>
-        <script src='${serverConfig.env === 'production' ? assetsManifest['/vendor.js'] : '/vendor.js'}'></script>
-        <script src='${serverConfig.env === 'production' ? assetsManifest['/app.js'] : '/app.js'}'></script>
+        <script src='${serverConfig.env === 'production'
+          ? assetsManifest['/vendor.js'] : '/vendor.js'
+        }'></script>
+        <script src='${serverConfig.env === 'production'
+          ? assetsManifest['/app.js'] : '/app.js'
+        }'></script>
       </body>
     </html>
   `
