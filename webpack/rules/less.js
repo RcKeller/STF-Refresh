73a666daef5cc4f8d701bb354cfe343ea7dbc5ef
Ryan Keller <rykeller@uw.edu>
2017-05-29 02:01:40 -0700

    YES! Got less to loadyarn devyarn dev Be advised installing via yarn fails if package.json is opened in a root program...e.g atom.

--- webpack/rules/less.js
+++ webpack/rules/less.js
@@ -2,12 +2,9 @@
 LESS is used to process ant design components and associated themes.
 I would recommend against using it outside of that case (keep styles consistent!)
 */
-module.exports = () => ({
+// const PATHS = require('../paths');
+module.exports = ({ production = false, browser = false } = {}) => ({
     test: /\.less$/,
-    loaders: [
-      "style-loader",
-      {loader: 'css-loader', options: {sourceMap: 1}},
-      "postcss-loader",
-      "less-loader"
-    ]
+    use: [ 'style-loader', 'css-loader', 'less-loader' ],
+    // include: [PATHS.app, PATHS.modules]
 });
