c9ac6e77a81840c57a7f529efaaff425bbba4858
Ryan Keller <rykeller@uw.edu>
2017-05-28 19:43:50 -0700

    Successfully configured passport-uwshib, excluded ES6 trickery.

--- server/init/passport/uw.js
+++ server/init/passport/uw.js
@@ -1,27 +1,19 @@
-// import { OAuth2Strategy as GoogleStrategy } from 'passport-google-oauth';
-// import shib from 'passport-uwshib';
-// var shib = require('passport-uwshib');
-// import * as shib from 'passport-uwshib'
-// import shib = require('passport-uwshib'); // it really only works if import = notation is used
-
 import db from '../../db';
 import config from 'config';
 
 export default (passport) => {
   try {
-    const shib = require('passport-uwshib').default;
-
     const domain = `${config.get('domain')}:${config.get('port')}`
     const url = `${config.get('protocol')}://${domain}`
-    
-    passport.use(
-      new shib.Strategy({
-        entityId: url,
-        privateKey: config.get('key'),
-        callbackURL: config.get('uw.callbackURL'),
-        domain
-      })
-    );
+    //  Passport-UWShib is using commonJS, so don't try adding ES6 trickery here.
+    var Shibboleth = require('passport-uwshib');
+    var UWStrategy = new Shibboleth.Strategy({
+      entityId: url,
+      privateKey: config.get('key'),
+      callbackURL: config.get('uw.callbackURL'),
+      domain
+    })
+    passport.use(UWStrategy);
   } catch (err) {
     console.warn(`ERROR: UW Shib strategy failed to initialize. Auth WILL be broken.\n${err}`)
   }
