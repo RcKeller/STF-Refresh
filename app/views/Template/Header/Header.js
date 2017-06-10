023b703f364291bdb80a1b3c88d5b913cb863e22
Ryan Keller <rykeller@uw.edu>
2017-06-09 23:19:40 -0700

    Adding SVG.

--- app/views/Template/Header/Header.js
+++ app/views/Template/Header/Header.js
@@ -5,9 +5,17 @@ import Helmet from 'react-helmet'
 
 import { Layout } from 'antd'
 const { Header: AntHeader } = Layout
-
+// import uwPatch from './Header.svg';
+import w from '../../../images/w.svg';
+import uw from '../../../images/uw.svg';
+// import letter from './Letter.svg';
+// import logo from './Logo.svg';
 const Header = () => (
   <AntHeader>
+    <div height='60' width='200'>
+    {w}
+    <img src={uw} />
+    </div>
     UW & STF Header here.
   </AntHeader>
 )
--- webpack/rules/image.js
+++ webpack/rules/image.js
@@ -1,7 +1,7 @@
 const PATHS = require('../paths')
 
 module.exports = ({ limit = 10000 } = {}) => ({
-  test: /\.(png|jpg|jpeg|gif|svg|woff|woff2|ico)$/,
+  test: /\.(png|jpg|jpeg|gif|svg|woff|woff2|ico|svg)$/,
   loader: 'url-loader',
   options: { name: '[hash].[ext]', limit },
   include: PATHS.app
