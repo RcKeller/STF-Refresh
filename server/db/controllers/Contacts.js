9978a380fd7b2068494b55ba895fb3e2f5f8064b
Ryan Keller <rykeller@uw.edu>
2017-06-10 20:35:13 -0700

    About to scaffold APIs

--- server/db/controllers/Contacts.js
+++ server/db/controllers/Contacts.js
@@ -1,59 +1,47 @@
 import passport from 'passport'
 import Contact from '../models/Contact'
 
-/**
- * List
- */
+/* *****
+  List
+***** */
 export function all (req, res) {
   Contact.find({}).exec((err, Contacts) => {
-    if (err) {
-      console.log('Error in first query')
-      return res.status(500).send('Something went wrong getting the data')
-    }
+    if (err) { return res.status(500).send('Server error retrieving all Contact documents') }
     return res.json(Contacts)
   })
 }
 
-/**
- * Add a Contact
- */
+/* *****
+  Add a Contact
+***** */
 export function add (req, res) {
   Contact.create(req.body, (err) => {
-    if (err) {
-      console.log(err)
-      return res.status(400).send(err)
-    }
+    if (err) { return res.status(400).send(err) }
     return res.status(200).send('OK')
   })
 }
 
-/**
- * Update a Contact
- */
+/* *****
+  Update a Contact
+***** */
 export function update (req, res) {
   const query = { id: req.params.id }
   const omitKeys = ['id', '_id', '_v']
   const data = _.omit(req.body, omitKeys)
 
   Contact.findOneAndUpdate(query, data, (err) => {
-    if (err) {
-      console.log('Error on save!')
-      return res.status(500).send('We failed to save for some reason')
-    }
+    if (err) { return res.status(500).send('Server error saving Contact') }
     return res.status(200).send('Updated successfully')
   })
 }
 
-/**
- * Remove a Contact
- */
+/* *****
+  Remove a Contact
+***** */
 export function remove (req, res) {
   const query = { id: req.params.id }
   Contact.findOneAndRemove(query, (err) => {
-    if (err) {
-      console.log('Error on delete')
-      return res.status(500).send('We failed to delete for some reason')
-    }
+    if (err) { return res.status(500).send('Server error deleting Contact') }
     return res.status(200).send('Removed Successfully')
   })
 }
