e6adb72921015da503900fcef49693c7d48937da
Ryan Keller <rykeller@uw.edu>
2017-06-22 20:05:07 -0700

    Working on refactoring and all CRUD ops.

--- app/services/api.js
+++ app/services/api.js
@@ -1,4 +1,26 @@
 import { API, version } from './environment'
+//  API Mutators are wrapped with async middleware
+import { mutateAsync } from 'redux-query'
+
+/*
+EXAMPLE IMPLEMENTATION:
+@compose(
+  connect(
+    state => ({ proposal: state.entities.proposal }),
+    dispatch => ({ api: bindActionCreators(api, dispatch)
+    })
+  ),
+  connectRequest(() => api.get('proposal', '594b49998dabd50e2c71762d', {
+    populate: 'body,decision'
+  }))
+)
+...
+render ({ proposal, api } = this.props) {
+...call like any other standard function.
+*/
+
+// dispatch => ({ actions: bindActionCreators(api, dispatch)
+
 /*
 FOREWARD:
 This file can be a little confusing, but here's what we're doing:
@@ -8,13 +30,13 @@ B) Initializing an API, which will hold methods for accessing each of those.
 //  TODO: create a querystring adapter that is API specific, packages don't work.
 
 /*
-GET all models
+GET ALL
 ex: api.getAll('proposal', { populate: 'contacts,decision' })
 Can use complex queries as an obj
 */
 const getAll = (model, query) => ({
   url: `${API}/${version}/${model}`,
-  'options.method': 'GET',
+  options: { method: 'GET' },
   // Assign response to prop.
   transform: body => ({ [`${model}s`]: body }),
   body: { ...query },
@@ -29,7 +51,7 @@ ex: api.get('proposal', '594b49998dabd50e2c71762d')
 */
 const get = (model, id, query) => ({
   url: `${API}/${version}/${model}/${id}`,
-  'options.method': 'GET',
+  options: { method: 'GET' },
   //  Assign response to prop.
   transform: body => ({ [`${model}`]: body }),
   body: { ...query },
@@ -37,9 +59,14 @@ const get = (model, id, query) => ({
   update: { [`${model}`]: (prev, next) => next }
 })
 
-const post = (model, body) => ({
+/*
+CREATE: POST
+Pass the object in as the body arg
+ex: api.post('report', {})
+*/
+const post = (model, body) => mutateAsync({
   url: `${API}/${version}/${model}`,
-  'options.method': 'POST',
+  options: { method: 'POST' },
   //  Assign response to prop.
   transform: body => ({ [`${model}`]: body }),
   // body: { ...query },
@@ -48,13 +75,42 @@ const post = (model, body) => ({
   update: { [`${model}`]: (prev, next) => next }
 })
 
+/*
+UPDATE: PUT
+ex: api.put('report', '594b49998dabd50e2c7176bf',
+{ date: "2000-06-21T07:15:10.746Z" })
+*/
+const put = (model, id, body) => mutateAsync({
+  url: `${API}/${version}/${model}/${id}`,
+  options: { method: 'PUT' },
+  //  Assign response to prop.
+  transform: body => ({ [`${model}`]: body }),
+  body,
+  //  Update the models with next state
+  update: { [`${model}`]: (prev, next) => next }
+})
+
+/*
+DELETE
+ex: api.remove('report', '594b49998dabd50e2c7176bf')
+*/
+const remove = (model, id) => mutateAsync({
+  url: `${API}/${version}/${model}/${id}`,
+  options: { method: 'DELETE' },
+  //  Assign response to prop.
+  transform: body => ({ [`${model}`]: body }),
+  //  Update the models with next state
+  update: { [`${model}`]: (prev, next) => next }
+})
+
 export default {
   //  Read (can use with connectRequest())
   getAll,
   get,
-  //  CUD: Need to use mutateAsync(), can promise chain (...then(res => ...))
   //  Create
-  post
+  post,
   //  Update
+  put,
   //  Delete
+  remove
 }
--- app/views/Proposals/Proposal/Proposal.js
+++ app/views/Proposals/Proposal/Proposal.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 
-import { compose } from 'redux'
+import { compose, bindActionCreators } from 'redux'
 import { connect } from 'react-redux'
 import { connectRequest } from 'redux-query'
 
@@ -9,13 +9,17 @@ import api from '../../../services'
 
 import styles from './Proposal.css'
 @compose(
-  connect(state => ({ proposal: state.entities.proposal })),
+  connect(
+    state => ({ proposal: state.entities.proposal }),
+    dispatch => ({ api: bindActionCreators(api, dispatch)
+    })
+  ),
   connectRequest(() => api.get('proposal', '594b49998dabd50e2c71762d', {
     populate: 'body,decision,contacts,manifests,reports,amendments,comments,'
   }))
 )
 class Proposal extends React.Component {
-  render ({ proposal } = this.props) {
+  render ({ proposal, actions } = this.props) {
     return (
       <article className={styles['article']}>
         {proposal &&
@@ -24,6 +28,7 @@ class Proposal extends React.Component {
             <h2>{proposal.title}</h2>
             <hr />
             <p>{JSON.stringify(proposal)}</p>
+            {/* <button style={{padding: 20}} onClick={(proposal) => post(proposal)} /> */}
           </div>
         }
       </article>
