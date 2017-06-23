a2d62681b5b62556d841e19861d518f9729feefb
Ryan Keller <rykeller@uw.edu>
2017-06-23 15:45:45 -0700

    Phew, what a day. We're transitioning redux-form out and using antd's form system. It's heavy but provides predictable controls and lets us handle the lifecycle of patching.

--- app/views/Proposals/Create/Create.js
+++ app/views/Proposals/Create/Create.js
@@ -1,5 +1,10 @@
 import React from 'react'
 import PropTypes from 'prop-types'
+import { compose, bindActionCreators } from 'redux'
+import { connect } from 'react-redux'
+import { connectRequest } from 'redux-query'
+
+import api from '../../../services'
 
 import Introduction from './Introduction/Introduction'
 // import Overview from './Overview/Overview'
@@ -7,134 +12,116 @@ import Introduction from './Introduction/Introduction'
 // import Manifest from './Manifest/Manifest'
 // import Signatures from './Signatures/Signatures'
 
-import { Form, Steps, Icon, Button } from 'antd'
-const Step = Steps.Step
+import { Form, Icon, Button, Tabs, message } from 'antd'
+const TabPane = Tabs.TabPane
 const FormItem = Form.Item
-const ButtonGroup = Button.Group
-const AntForm = (component) => Form.create(component)
-
-// const AntForm = (component) => Form.create(component)
 
 function hasErrors (fields) {
   return Object.keys(fields).some(field => fields[field])
 }
-
-const steps = [
-  { title: 'Introduction', icon: 'team' },
-  { title: 'Overview', icon: 'solution' },
-  { title: 'Plan', icon: 'book' },
-  { title: 'Manifest', icon: 'wallet' },
-  { title: 'Signatures', icon: 'edit' }
-]
-// import validate from './validate'
-import styles from './Create.css'
-// @reduxForm({
-//   form: 'create',
-//   destroyOnUnmount: false,
-//   validate: validate
-// })
 
 /*
 connectRequest should get proposal by ID
 If there is no ID, or there is an ID but you're a contact...
 Render form components. Run validation and api.patch() to update the remote as necessary
 */
+import styles from './Create.css'
+@compose(
+  connect(
+    state => ({ proposal: state.entities.proposal }),
+    dispatch => ({ api: bindActionCreators(api, dispatch) })
+  ),
+  connectRequest(() => api.get('proposal', '594b49998dabd50e2c71762d', {
+    populate: 'body,decision,contacts,manifests,reports,amendments,comments,'
+  }))
+)
 class Create extends React.Component {
-  constructor (props) {
-    super(props)
-    //  Index - the step we're on.
-    this.state = { index: 0 }
-  }
-  // Disable submit button at the beginning.
-  componentDidMount () {
-    this.props.form.validateFields()
-  }
+  // Disable submit button at the beginning by running validation.
+  componentDidMount () { this.props.form.validateFields() }
   /*
   handleSubmit will handle the FINAL submission, which includes
   marking a proposal as complete / not a draft.
   */
-  handleSubmit = (e, direction) => {
+  handleSubmit = (e) => {
     e.preventDefault()
-    this.props.form.validateFields((err, values) => {
-      if (!err) console.log('FORM', values)
-    })
-  }
-  /*
-  Moving forwards or backwards triggers form validation
-  You can only move by validating your data.
-  This prevents stuff like signaure spoofing, etc
-  */
-  handleStep (e, direction) {
-    e.preventDefault()
-    this.props.form.validateFields((err, values) => {
+    let { form, proposal, api } = this.props
+    form.validateFields((err, values) => {
       if (!err) {
-        switch (direction) {
-          case 'next':
-            this.setState({ index: ++this.state.index })
-            break
-          case 'prev':
-            this.setState({ index: --this.state.index })
-            break
+        try {
+          api.patch('proposal', proposal._id, values)
+          message.success('Draft updated!')
+          //  TODO: Once we add a bool for if proposals are drafts, update this to reflect that.
+        } catch (err) {
+          message.error('An error occured - Draft failed to update')
+          console.warn(err)
         }
-        console.log('FORM', values)
       }
     })
   }
-  render ({handleSubmit, form} = this.props) {
-    const content = [
-      <Introduction {...form} />
-    ]
+  /*
+  Util for silently updating the proposal on the backend.
+  Leaving this be for now, until we decide on how aggressive our validation will be.
+  */
+  update () {
+    let { form } = this.props
+    form.validateFields((err, values) => {
+      if (!err) {
+        console.log('Update:', values)
+      } else {
+        console.warn('Update fails validation:', err)
+      }
+    })
+  }
+  render ({ handleSubmit, form, proposal } = this.props) {
     const invalidData = hasErrors(form.getFieldsError())
     return (
       <article className={styles['create']}>
-        <h1>Creating Proposal</h1>
-        <Steps index={this.state.index}>
-          {steps.map((s, i) => (
-            <Step key={i} title={s.title}
-              icon={<Icon type={s.icon} />}
-            />
-          ))}
-        </Steps>
-        <Form onSubmit={handleSubmit}>
-          <section className='steps-action'>
-            <ButtonGroup className={styles['button-container']}>
-              <Button size='large' type='primary'
-                className={styles['step-button']}
-                htmlType='submit'
-                disabled={invalidData || this.state.index === 0}
-                onClick={(e) => this.handleStep(e, 'prev')}
-              >
-                <Icon type='left' />Previous
-              </Button>
-              <Button size='large' type='primary'
-                className={styles['step-button']}
-                htmlType='submit'
-                disabled={invalidData || this.state.index >= steps.length - 1}
-                onClick={(e) => this.handleStep(e, 'next')}
-                >
-                  Next<Icon type='right' />
-              </Button>
-            </ButtonGroup>
-            <div>
-              {content[this.state.index]}
-            </div>
-            {this.state.index === steps.length - 1 &&
-              <FormItem className={styles['button-container']}>
-                <Button size='large' type='primary'
-                  className={styles['submit-button']}
-                  htmlType='submit'
-                  disabled={invalidData}
-                  >Submit</Button>
-                </FormItem>
-            }
-          </section>
+        <h1>{proposal ? proposal.title : 'Creating New Proposal'}</h1>
+        <Form onSubmit={this.handleSubmit}>
+          <Tabs defaultActiveKey='1' onChange={() => this.update()}>
+            <TabPane key='1' tab={<span>
+              <Icon type='team' />
+              Introduction</span>
+            }>
+              <Introduction {...form} />
+            </TabPane>
+            <TabPane key='2' tab={
+              <span><Icon type='solution' />Overview</span>
+            }>
+              <p>Overview</p>
+            </TabPane>
+            <TabPane key='3' tab={
+              <span><Icon type='book' />Plan</span>
+            }>
+              <p>Plan</p>
+            </TabPane>
+            <TabPane key='4' tab={
+              <span><Icon type='wallet' />Manifest</span>
+            }>
+              <p>Manifest</p>
+            </TabPane>
+            <TabPane key='5' tab={
+              <span><Icon type='edit' />Signatures</span>
+            }>
+              <p>Signatures</p>
+            </TabPane>
+          </Tabs>
+          <FormItem>
+            <Button size='large' type='primary'
+              className={styles['submit-button']}
+              htmlType='submit'
+              disabled={invalidData}
+            >Update</Button>
+          </FormItem>
         </Form>
       </article>
     )
   }
 }
 Create.propTypes = {
-  form: PropTypes.object
+  form: PropTypes.object,
+  proposal: PropTypes.object,
+  api: PropTypes.object
 }
 // Decoration doesn't work.
 const CreateForm = Form.create()(Create)
