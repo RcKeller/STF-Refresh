8e6c13e9ebb0f4427310e76d69d95102e41f0b67
Ryan Keller <rykeller@uw.edu>
2017-06-30 10:13:20 -0700

    Finished conditional render to prevent errors.

--- app/views/Blocks/Block/Block.js
+++ app/views/Blocks/Block/Block.js
@@ -7,7 +7,7 @@ import { connectRequest, querySelectors } from 'redux-query'
 
 import api from '../../../services'
 
-import { Spin, Collapse } from 'antd'
+import { Row, Col, Spin, Progress, Collapse, Card } from 'antd'
 const Panel = Collapse.Panel
 
 const capitalize = (word) => word[0].toUpperCase() + word.substr(1)
@@ -39,14 +39,33 @@ import styles from './block.css'
 )
 class Block extends React.Component {
   render ({ block, loading } = this.props) {
+    // let progressStatus = 'active'
+    // if (block.decision) switch(true) {
+    // // if (block.decision) switch(block.decision.approved) {
+    //   case (true):
+    //     progressStatus = 'success'
+    //   case (false):
+    //     progressStatus = 'exception'
+    // }
     return (
       <article className={styles['article']}>
         {loading || !block
           ? <Spin size='large' tip='Loading...' />
           : <div>
-            <h5>{block.year}-{block.number}</h5>
-            <h1>{block.title}</h1>
-            <h2>For {block.organization}</h2>
+            <Row type="flex" justify="space-between">
+              <Col key='intro' xs={24} md={20} xl={22}>
+                <h1>{block.title}</h1>
+                <h2>For {block.organization}</h2>
+              </Col>
+              <Col key='meta' xs={24} md={4} xl={2}>
+                <Progress type='circle' width={80}
+                  status={block.decision ? (block.decision.approved ? 'success' : 'exception') : 'active'}
+                  // status={block.decision && block.decision.approved ? 'success' : 'exception'}
+                />
+                <h5>Status: {block.status}</h5>
+                <p>{`ID: ${block.year}-${block.number}`}</p>
+              </Col>
+            </Row>
             <Collapse>
               {testContacts.map((contact, i) => (
                 <Panel key={i} header={`${capitalize(contact.role)} Contact: ${contact.name}, ${contact.title}`}>
@@ -59,12 +78,22 @@ class Block extends React.Component {
                 </Panel>
               ))}
             </Collapse>
-            <div>
+            <section>
               <h2>Project Overview</h2>
               <p>{block.body.overview.abstract}</p>
               <h3>Objectives</h3>
               <p>{block.body.overview.objectives}</p>
-            </div>
+            </section>
+            <section>
+              <h2>Ongoing Commitment</h2>
+              <br />
+              <h3>Services Provided</h3>
+              <p>{block.body.plan.state}</p>
+              <h3>Execution Strategy</h3>
+              <p>{block.body.plan.strategy}</p>
+              <h3>Risks and Mitigations</h3>
+              <p>{block.body.plan.risk}</p>
+            </section>
           </div>
         }
       </article>
