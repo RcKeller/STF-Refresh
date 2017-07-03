c9be4fac6422922e2080f4582d890574864f1fb2
Ryan Keller <rykeller@uw.edu>
2017-07-03 10:44:50 -0700

    Created ID gen object.

--- server/db/dummy.js
+++ server/db/dummy.js
@@ -1,3 +1,4 @@
+import mongoose from 'mongoose'
 import config from 'config'
 
 // import { dummyUsers } from './models/user'
@@ -7,7 +8,32 @@ export default function () {
   const min = config.has('lorem-ipsum') ? config.get('lorem-ipsum') : 5 // default
   console.log(`SEED: Lorem Ipsum Mode enabled. Seeding up to ${min} documents each...`)
   //  Activate dummy data generators, with specified minimums document counts.
+
+  //  Generate an object containing ObjectIds for dummy objects.
+  const ids = {
+    contact: [],
+    comment: [],
+    proposal: [],
+    project: [],
+    amendment: [],
+    manifest: [],
+    item: [],
+    block: [],
+    review: [],
+    decision: [],
+    report: []
+    //  User?
+  }
+  Object.keys(ids).forEach((key) => {
+    for (let i = 0; i < min; i++) {
+      ids[key].push(new mongoose.Types.ObjectId())
+    }
+  })
+  console.log(ids)
+
   //  BUG: Using these accounts makes auth fail. Unsure of cause.
   // dummyUsers()
+
+  //  Create dummies for all RESTful models
   restDummies.map((model) => model(min))
 }
