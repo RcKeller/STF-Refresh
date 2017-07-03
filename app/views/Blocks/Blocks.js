f23399c09a631275cff40bb3b051ab1f9db15e2e
Ryan Keller <rykeller@uw.edu>
2017-07-03 11:21:28 -0700

    Great! Router Link Render fixed.

--- app/views/Blocks/Blocks.js
+++ app/views/Blocks/Blocks.js
@@ -4,11 +4,15 @@ import PropTypes from 'prop-types'
 import { compose } from 'redux'
 import { connect } from 'react-redux'
 import { connectRequest } from 'redux-query'
+
 import api from '../../services'
 
+import { Link } from 'react-router'
 import { Spin, Table } from 'antd'
 
 import styles from './Blocks.css'
+// const renderTitle = (text, record) => <Link to={`/${record.number}`}>{record.title}</Link>
+
 @compose(
   connect(state => ({
     blocks: state.entities.blocks,
@@ -23,7 +27,11 @@ class Blocks extends React.Component {
       columns: [
         { title: 'Year', dataIndex: 'year', key: 'year' },
         { title: '#', dataIndex: 'number', key: 'number' },
-        { title: 'Title', dataIndex: 'title', key: 'title' },
+        { title: 'Title',
+          dataIndex: 'title',
+          key: 'title',
+          render: (text, record) => <Link to={`/blocks/${record.number}`}>{record.title}</Link>
+        },
         { title: 'Organization', dataIndex: 'organization', key: 'organization' },
         { title: 'Status', dataIndex: 'status', key: 'status' }
       ]
@@ -37,18 +45,11 @@ class Blocks extends React.Component {
       <article className={styles['blocks']}>
         {!blocks
           ? <Spin size='large' tip='Loading...' />
-          : <div>
-            <Table dataSource={blocks}
-              sort
-              size={
-                screen.lessThan.medium ? 'small' : ''
-              }
-              columns={
-                screen.lessThan.medium ? columns.slice(1, 3) : columns
-              }
-              title={() => <h1>Continuous Funding Blocks</h1>}
-            />
-          </div>
+          : <Table dataSource={blocks} sort
+            size={screen.lessThan.medium ? 'small' : ''}
+            columns={screen.lessThan.medium ? columns.slice(1, 3) : columns}
+            title={() => <h1>Continuous Funding Blocks</h1>}
+          />
         }
       </article>
     )
