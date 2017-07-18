08e916358cd5428488b8450ca09d5b4985724677
Ryan Keller <rykeller@uw.edu>
2017-07-17 19:48:19 -0700

    It works, but simply finding the role by  index is not enough. Need util func.

--- app/views/Proposals/Edit/Contacts/Contact/Contact.js
+++ app/views/Proposals/Edit/Contacts/Contact/Contact.js
@@ -33,41 +33,31 @@ class Contact extends React.Component {
     }
     form.validateFields()
   }
-  // handleSubmit = (e) => {
-  //   e.preventDefault()
-  //   let { form, api, parent, contact } = this.props
-  //   form.validateFields((err, values) => {
-  //     if (!err) {
-  //       console.log(values)
-  //       values = values.contacts
-  //       //  Student contacts are optional. Remove their fields if not completed.
-  //       const student = 3
-  //       if (!values[student] || !values[student].name || !values[student].netID) {
-  //         values.splice(student, 1)
-  //       }
-  //       //  Iterate over each contact, setting roles as appropriate.
-  //       values.forEach((contact, i) => {
-  //         values.role = contactFields[i].field
-  //         //  Patch contacts if they already exist. Create them otherwise.
-  //         contacts[i]
-  //         ? api.patch(
-  //           'contact',
-  //           { proposal: parent, ...values[i] },
-  //           { id: contacts[i]._id }
-  //         )
-  //         : api.post(
-  //           'contact',
-  //           { proposal: parent, ...values[i] }
-  //         )
-  //         .then(message.success('Updated ${contactFields[i].title}!'))
-  //         .catch(err => {
-  //           message.warning('Failed to update ${contactFields[i].title} - Unexpected client error')
-  //           console.warn(err)
-  //         })
-  //       })
-  //     }
-  //   })
-  // }
+  handleSubmit = (e) => {
+    e.preventDefault()
+    let { form, api, parent, contact, role, title } = this.props
+    form.validateFields((err, values) => {
+      if (!err && values) {
+        //  Patch contacts if they already exist. Create them otherwise.
+        values.role = role
+        contact
+        ? api.patch(
+          'contact',
+          { proposal: parent, ...values },
+          { id: contact._id }
+        )
+        : api.post(
+          'contact',
+          { proposal: parent, ...values }
+        )
+        .then(message.success(`Updated ${title} !`))
+        .catch(err => {
+          message.warning(`Failed to update ${title} - Unexpected client error`)
+          console.warn(err)
+        })
+      }
+    })
+  }
 
   render ({ form, contact, index, role, title, subtitle } = this.props) {
     //  If this
@@ -110,67 +100,16 @@ class Contact extends React.Component {
     )
   }
 }
-/*
-<h1>Contact Information</h1>
-<Row gutter={32}>
-  {contactFields.map((c, i) => (
-    <Col key={i} className='gutter-row' xs={24} md={12} lg={6} >
-      <h3>{c.title}</h3>
-      <p>{c.subtitle}</p>
-      <FormItem
-        hasFeedback={feedback(form, 'name')}
-        help={help(form, 'name')}
-      >
-        {form.getFieldDecorator('name', requireStaff)(
-          <Input prefix={<Icon type='edit' style={jss.icon} />} placeholder='Name' />
-        )}
-      </FormItem>
-      <FormItem
-        hasFeedback={feedback(form, 'netID')}
-        help={help(form, 'netID')}
-      >
-        {form.getFieldDecorator('netID', requireStaff)(
-          <Input prefix={<Icon type='idcard' style={jss.icon} />} placeholder='NetID' />
-        )}
-      </FormItem>
-      <FormItem
-        hasFeedback={feedback(form, 'title')}
-        help={help(form, 'title')}
-      >
-        {form.getFieldDecorator('title', requireStaff)(
-          <Input prefix={<Icon type='info-circle-o' style={jss.icon} />} placeholder='Title' />
-        )}
-      </FormItem>
-      <FormItem
-        hasFeedback={feedback(form, 'phone')}
-        help={help(form, 'phone')}
-      >
-        {form.getFieldDecorator('phone', requireStaff)(
-          <Input prefix={<Icon type='phone' style={jss.icon} />} placeholder='Phone' />
-        )}
-      </FormItem>
-      <FormItem
-        hasFeedback={feedback(form, 'mailbox')}
-        help={help(form, 'mailbox')}
-      >
-        {form.getFieldDecorator('mailbox')(
-          <Input prefix={<Icon type='inbox' style={jss.icon} />} placeholder='Mailbox #' />
-        )}
-      </FormItem>
-    </Col>
-  ))}
-</Row>
-<FormItem>
-  <Button size='large' type='primary' width='100%' style={{width: '100%'}}
-    htmlType='submit' disabled={disableSubmit(form)}
-    >Update</Button>
-</FormItem>
-*/
 
 Contact.propTypes = {
   form: PropTypes.object,
   api: PropTypes.object,
   parent: PropTypes.string,
-  contact: PropTypes.object
+  contact: PropTypes.object,
+  //  Props from container - REQUIRED.
+  role: PropTypes.string,
+  index: PropTypes.number,
+  title: PropTypes.string,
+  subtitle: PropTypes.string
 }
 export default Contact
