3d1d226939aedccdc4c098d4ba9d91f2564b8741
Ryan Keller <rykeller@uw.edu>
2017-07-18 17:35:00 -0700

    GREAT! CRUD is working.

--- app/views/Committee/Knowledge/Article/Update/Update.js
+++ app/views/Committee/Knowledge/Article/Update/Update.js
@@ -1,33 +1,68 @@
-import React from 'react';
-import RichTextEditor from 'react-rte';
+import React from 'react'
+import PropTypes from 'prop-types'
 
+import { bindActionCreators } from 'redux'
+import { connect } from 'react-redux'
+
+import api from '../../../../../services'
+
+import RichTextEditor from 'react-rte'
+import { Button, message } from 'antd'
+
+@connect(
+  state => ({
+    id: state.db.article._id,
+    body: state.db.article.body
+  }),
+  dispatch => ({ api: bindActionCreators(api, dispatch) })
+)
 class Update extends React.Component {
   constructor (props) {
     super(props)
     this.state = {
-      value: RichTextEditor.createEmptyValue()
+      value: RichTextEditor.createValueFromString(props.body, 'html'),
+      loading: false
     }
   }
 
-  onChange = (value) => {
-    this.setState({ value })
-    if (this.props.onChange) {
-      // Send the changes up to the parent component as an HTML string.
-      // This is here to demonstrate using `.toString()` but in a real app it
-      // would be better to avoid generating a string on each change.
-      this.props.onChange(
-        value.toString('html')
-      )
-    }
-  };
+  onChange = (value) => this.setState({ value })
 
-  render () {
+  handleUpdate = () => {
+    this.setState({ loading: true })
+    const body = this.state.value.toString('html')
+    const { api, id } = this.props
+    console.log('Uploading value', body)
+    api.patch(
+      'article',
+      { body },
+      { id }
+    )
+    .then(message.success(`Updated!`))
+    .catch(err => {
+      message.warning(`Update failed - Unexpected error`)
+      console.warn(err)
+    })
+    .then(this.setState({ loading: false }))
+  }
+  render ({ value } = this.state) {
     return (
-      <RichTextEditor
-        value={this.state.value}
-        onChange={this.onChange}
-      />
+      <section>
+        <RichTextEditor
+          value={this.state.value}
+          onChange={this.onChange}
+        />
+        <Button size='large' type='primary'
+          loading={this.state.loading}
+          onClick={this.handleUpdate}
+        >Update</Button>
+      </section>
     )
   }
+}
+
+Update.propTypes = {
+  api: PropTypes.object,
+  id: PropTypes.string,
+  body: PropTypes.string
 }
 export default Update
