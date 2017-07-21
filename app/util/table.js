e4001fc1d307fc26e4a31829bff6458c331590e5
Ryan Keller <rykeller@uw.edu>
2017-07-21 11:19:10 -0700

    Code cleanup.

--- app/util/table.js
+++ app/util/table.js
@@ -4,6 +4,8 @@ import PropTypes from 'prop-types'
 import { Table, Input, InputNumber, Switch, Button, Icon } from 'antd'
 const { Group } = Button
 
+const uuidv4 = require('uuid/v4')
+
 class EditableCell extends React.Component {
   constructor (props) {
     super(props)
@@ -14,8 +16,8 @@ class EditableCell extends React.Component {
   }
   componentWillReceiveProps (nextProps) {
     //  Editing disabled - update data via callback and clear cache.
-    const { editable, onChange } = this.props
-    if (editable && !nextProps.editable) {
+    const { editing, onChange } = this.props
+    if (editing && !nextProps.editing) {
       onChange(this.state.value)
     }
   }
@@ -23,12 +25,15 @@ class EditableCell extends React.Component {
   render (
     { Edit, Cell, handleChange } = this,
     { value } = this.state,
-    { editable } = this.props
+    { editing } = this.props
   ) {
     return (
       <div>
-        {editable
-          ? <Edit size='large' value={value} handleChange={this.handleChange} />
+        {editing
+          ? <Edit
+            value={value} handleChange={this.handleChange}
+            size='large'
+          />
           : <Cell value={value} />
         }
       </div>
@@ -46,10 +51,9 @@ class EditableTable extends React.Component {
     super(props)
     let { columns, dataSource } = this.props
     //  Add unique keys to your data. These serve as unique identifiers.
-    //  Then set editing to false. Double clicking activates a callback, inverting this value.
+    //  We're just using a loop for compute efficiency, uuid's can be used later.
     for (let i = 0; i < dataSource.length; i++) {
       dataSource[i]._key = i
-      dataSource[i]._editable = false
     }
     //  Apply custom renderers that enable data editing.
     columns.forEach((column, i) => {
@@ -60,43 +64,34 @@ class EditableTable extends React.Component {
         this.renderColumn(text, record, column.dataIndex, components)
     })
     this.columns = columns
-    this.state = { data: dataSource, editAll: false }
+    this.state = { data: dataSource, editing: false }
   }
   //  RenderColumn passes cell data and callbacks to display elements.
   renderColumn = (text, record, dataIndex, components) => {
-    const { _editable, _key } = record
+    const { _key } = record
+    const { editing } = this.state
     const callback = value => this.handleChange(dataIndex, _key, value)
     return <EditableCell
-      editable={_editable}
+      editing={editing}
       value={text} components={components}
       onChange={callback}
     />
   }
-  //  ToggleRow finds the record with the specified key and sets it to editable.
-  // toggleEditRow = (record) => {
-  //   let { data } = this.state
-  //   const key = record._key
-  //   data.forEach((d, i) => {
-  //     if (d._key === key) {
-  //       data[i]._editable = !record._editable
-  //       this.setState({ data })
-  //     }
-  //   })
-  // }
   toggleEditAll = () => {
-    let { data, editAll } = this.state
-    editAll = !editAll
+    let { data, editing } = this.state
+    editing = !editing
     for (let record of data) {
       // record._editable = !record._editable
-      record._editable = editAll
+      record._editable = !editing
     }
-    this.setState({ data, editAll })
-    console.log('Placeholder func')
+    this.setState({ data, editing })
+    // console.log('Placeholder func')
   }
   addRow = () => {
+    //  Add unique ID's
     let { data } = this.state
-    //  TODO: include uuidv4
-    data.push({ _editable: true })
+    const _key = uuidv4()
+    data.push({ _key })
     this.setState({ data })
   }
   deleteRow = () => {
@@ -115,12 +110,15 @@ class EditableTable extends React.Component {
   }
   //  Handlesubmit scrubs out _key and _editable when submitting to parent.
   handleSubmit = () => {
+    //  Set state to false, which will also force subcomponents to save changes to table data.
+    this.setState({ editing: false })
     let { data } = this.state
     const { onSubmit } = this.props
+    //  Remove unique ID's (keys) from data.
     const values = data.map((item) => {
       const obj = {}
       Object.keys(item).forEach((key) => {
-        if (key !== '_key' && key !== '_editable') {
+        if (key !== '_key') {
           obj[key] = item[key]
         }
       })
@@ -128,31 +126,30 @@ class EditableTable extends React.Component {
     })
     onSubmit(values)
   }
-  footer = () => (
-    <div>
-      <p>
-        <em>Double click a row to edit it.</em>
-      </p>
-      <Group size='large'>
-        <Button size='large' type='primary' ghost onClick={this.toggleEditAll}>
-          <Icon type='edit' />Edit All
-        </Button>
-        <Button size='large' type='primary' ghost onClick={this.addRow}>
-          <Icon type='plus-circle-o' />Add Row
-        </Button>
-        <Button size='large' type='primary' ghost onClick={this.handleSubmit}>
-          <Icon type='upload' />Update
-        </Button>
-      </Group>
-
-    </div>
-  )
   render (
-    { title, footer, columns, toggleEditRow, handleSubmit } = this,
+    { columns, toggleEditAll, addRow, handleSubmit } = this,
     { data } = this.state
   ) {
-    return <Table footer={footer}
-      bordered size='small' pagination={false}
+    const footer = () => (
+      <div>
+        <p>
+          <em>Double click a row to edit it.</em>
+        </p>
+        <Group size='large'>
+          <Button size='large' type='primary' ghost onClick={toggleEditAll}>
+            <Icon type='edit' />Edit All
+          </Button>
+          <Button size='large' type='primary' ghost onClick={addRow}>
+            <Icon type='plus-circle-o' />Add Row
+          </Button>
+          <Button size='large' type='primary' ghost onClick={handleSubmit}>
+            <Icon type='upload' />Update
+          </Button>
+        </Group>
+      </div>
+    )
+    return <Table bordered size='small' pagination={false}
+      footer={footer}
       dataSource={data}
       columns={columns}
       // onRowDoubleClick={toggleEditRow}
