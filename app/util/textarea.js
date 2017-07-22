5919adc419369e70c8cb309afe39cea0a303354b
Ryan Keller <rykeller@uw.edu>
2017-07-21 19:08:33 -0700

    Going to try using the source editors which have been refactored.

--- app/util/textarea.js
+++ app/util/textarea.js
@@ -2,11 +2,22 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import editorWrapper from './editorWrapper'
 import styles from './textarea.css'
+
+import { Input } from 'antd'
+const { TextArea } = Input
+
 const TextAreaEditor = ({ editorValue, onValueChanged }) => {
   return (
     // <div className='editor-main editor-base'>
     <div className={styles['editor-main, editor-base']}>
-      <textarea className='text-area' value={editorValue} onChange={({ target: { value } }) => { onValueChanged(value) }} />
+      {/* <textarea
+        className='text-area' value={editorValue}
+        onChange={({ target: { value } }) => { onValueChanged(value) }} /> */}
+      <TextArea rows={4}
+        value={editorValue}
+        onChange={e => console.log(e)}
+      />
+      {/* <textarea className='text-area' value={editorValue} onChange={({ target: { value } }) => { onValueChanged(value) }} /> */}
     </div>
   )
 }
