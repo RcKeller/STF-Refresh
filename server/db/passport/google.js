31ee5abff212f26ab0691a8c3a8d5e732d343901
Ryan Keller <rykeller@uw.edu>
2017-07-25 10:15:29 -0700

    Going to rewrite google auth, it's terribad.

--- server/db/passport/google.js
+++ server/db/passport/google.js
@@ -2,18 +2,37 @@ import User from '../models/user'
 
 /* eslint-disable no-param-reassign */
 export default (req, accessToken, refreshToken, profile, done) => {
-  // console.log('===PROFILE===', profile.id, profile._json)
+  let p = profile._json
+  let userData = {
+    name: p.displayName,
+    netID: p.emails[0].value,
+    email: p.emails[0].value,
+    google: p.id,
+    tokens: { kind: 'google', accessToken }
+  }
+  console.log('USERDATA', userData)
+// User is already logged in.
+// Check if there is an existing account with a provider id.
+// If there is, return an error message. (Account merging not supported)
+// Else link new OAuth account with currently logged-in user.
+// User is not logged in.
+// Check if it's a returning user.
+// If returning user, sign in and we are done.
+// Else check if there is an existing account with user's email.
+// If there is, return an error message.
+// Else create a new account.
+  console.log('===PROFILE===', profile.id, profile._json)
   const email = profile._json.emails[0].value
   const netID = email.split('@')[0]
   if (req.user) {
     // return User.findOne({ google: profile.id }, (findOneErr, existingUser) => {
     return User.findOne({ netID }, (findOneErr, existingUser) => {
       if (existingUser) {
-        // console.log('existingUser', existingUser)
+        console.log('existingUser by netID', existingUser)
         return done(null, false, { message: 'There is already a Google account that belongs to you. Sign in with that account or delete it, then link it with your current account.' })
       }
       return User.findById(req.user.id, (findByIdErr, user) => {
-        // console.log('found by id', req.user.id, user)
+        console.log('found by id', req.user.id, user)
         const parsedEmail = profile._json.emails[0].value
         user.name = profile.displayName
         user.netID = parsedEmail.split('@')[0]
@@ -28,7 +47,7 @@ export default (req, accessToken, refreshToken, profile, done) => {
   }
   return User.findOne({ netID }, (findByGoogleIdErr, existingUser) => {
     if (existingUser) {
-      // console.log('existingUser', existingUser)
+      console.log('existingUser by googleID', existingUser)
       return done(null, existingUser)
     }
     return User.findOne({ netID }, (findByEmailErr, existingEmailUser) => {
