c2a2e710d40bd0feb0a865339830bf0de35570c4
Ryan Keller <rykeller@uw.edu>
2017-08-16 12:45:54 -0700

    Very satisfied by how this is coming. Reviews.js may be getting bloated though.

--- app/views/STF/Voting/Vote/Review/Review.js
+++ app/views/STF/Voting/Vote/Review/Review.js
@@ -18,7 +18,7 @@ class SliderAndNumber extends React.Component {
     return (
       <Row>
         <Col span={12}>
-          <Slider {...this.props}  />
+          <Slider {...this.props} />
         </Col>
         <Col span={4}>
           <InputNumber {...this.props} style={{ marginLeft: 16 }} />
@@ -34,11 +34,11 @@ const questions = ['Proposal Quality', 'Academic Merit']
 @compose(
   connect(
     (state, props) => ({
+      proposal: state.db.manifests[props.index].proposal._id,
       manifest: state.db.manifests[props.index],
-      review: state.db.manifests[props.index].reviews.filter(review =>
-        // TODO: Remove placeholder for testing
-          // review.author._id === state.user._id),
-          review.author._id === "5991d88bae3e6f4ad0669bbf")[0] || {},
+      review: state.db.manifests[props.index].reviews.find(review =>
+          review.author._id === state.user._id
+        ) || {},
       user: state.user
     }),
     dispatch => ({ api: bindActionCreators(api, dispatch) })
@@ -46,35 +46,6 @@ const questions = ['Proposal Quality', 'Academic Merit']
   connectForm
 )
 class Review extends React.Component {
-  componentDidMount () {
-    const { form, review } = this.props
-    if (form && review) {
-      //  Consistent fields
-      const { score, approved } = review
-      //  Dynamic fields - rating prompts change all the time. Normalize
-      let qa = {}
-      for (const rating of review.ratings) {
-        qa[rating.prompt] = rating.score
-      }
-      let fields = { score, approved, ...qa }
-      //  TODO: Remove this test
-      fields[questions[0]] = 77
-      form.setFieldsValue(fields)
-    }
-  }
-  // handleCategory = (category) => {
-  //   const { api, id } = this.props
-  //   const update = {  //  Replace publication status only.
-  //     proposal: (prev, next) =>
-  //       Object.assign(prev, { organization: next.category })
-  //   }
-  //   api.patch('proposal', { category }, { id, update })
-  //   .then(message.success(`Updated category: ${category}`), 10)
-  //   .catch(err => {
-  //     message.warning(`Failed to update - client error`)
-  //     console.warn(err)
-  //   })
-  // }
   constructor (props) {
     super(props)
     const filter = { admin: true, member: true, spectator: true }
@@ -85,11 +56,9 @@ class Review extends React.Component {
     const filter = Object.assign(this.state.filter, checked)
     this.setState({ filter })
   }
-  render (
-    { form, manifest } = this.props,
-    { filter } = this.state
-  ) {
-    const { metrics, voting } = manifest.docket
+  filterReviews = () => {
+    const { form, manifest } = this.props
+    const { filter } = this.state
     //  All reviews, filtered and sorted by type (will have duplicates across keys, STF members have many roles)
     const reviews = {
       admin: manifest.reviews.filter(rev => filter.admin && rev.author.stf.admin === true),
@@ -104,56 +73,120 @@ class Review extends React.Component {
       ...reviews.member,
       ...reviews.spectator
     ])]
-    // console.warn('summary', filteredReviews)
-    // const scores = filteredReviews.reduce(function (totals, score) {
-    //   console.log('TOTALS', totals)
-    //   console.log('SCORE', score)
-    //   return totals
-    // }, {})
-    // console.warn('SCORES', scores)
+    console.warn('summary', filteredReviews)
+  }
+  componentDidMount () {
+    const { form, review } = this.props
+    if (form && review) {
+      //  Consistent fields
+      const { score, approved } = review
+      //  Dynamic fields - metric prompts change all the time. Normalize
+      //  rc-form format: { metrics: { prompt: score }}
+      let metrics = { }
+      for (const q of review.ratings) {
+        metrics[q.prompt] = q.score
+      }
+      let fields = { score, approved, metrics }
+      form.setFieldsValue(fields)
+    }
+  }
+  handleSubmit = (e) => {
+    e.preventDefault()
+    let { form, api, proposal, manifest, review, user } = this.props
+    form.validateFields((err, values) => {
+      if (!err) {
+        console.warn('Submitting', values)
+        const { metrics, score, approved } = values
+        let denormalizedMetrics = []
+        //  Denormalize prompts into scores: [{ prompt, score }]
+        Object.keys(metrics).forEach((key, i) => {
+          denormalizedMetrics.push({ prompt: key, score: metrics[key] })
+        })
+        const submission = {
+          proposal,
+          manifest: manifest._id,
+          author: user._id,
+          ratings: denormalizedMetrics,
+          score,
+          approved
+        }
+        console.warn('Review', submission)
+        //  TODO: Add custom update func
+        review._id
+          ? api.patch('review', submission, { id: review._id })
+          .then(message.success('Review updated!'))
+          .catch(err => {
+            message.warning('Review failed to update - Unexpected client error')
+            console.warn(err)
+          })
+          : api.post('review', submission)
+          .then(message.success('Review posted!'))
+          .catch(err => {
+            message.warning('Review failed to post - Unexpected client error')
+            console.warn(err)
+          })
+      }
+    })
+  }
+  render (
+    { form, manifest } = this.props,
+    { filter } = this.state
+  ) {
+    const { metrics, voting } = manifest.docket
+    this.filterReviews()
     return (
       <section>
         {!manifest
           ? <Spin size='large' tip='Loading...' />
-          : <div>
-            <h2>Average Scores</h2>
-            <h4>Filter by Commitee Roles</h4>
-            <Switch checked={filter.admin}
-              unCheckedChildren='Admins' checkedChildren='Admins'
-              onChange={admin => this.handleFilter({ admin })}
-            />
-            <Switch checked={filter.member}
-              unCheckedChildren='Members' checkedChildren='Members'
-              onChange={member => this.handleFilter({ member })}
-            />
-            <Switch checked={filter.spectator}
-              unCheckedChildren='Spectators' checkedChildren='Spectators'
-              onChange={spectator => this.handleFilter({ spectator })}
-            />
-            <h2>Your Review</h2>
-            {questions.map((q, i) => (
-              <FormItem key={i} label={q} {...layout} >
-                {form.getFieldDecorator(q)(
-                  // FIXME: Disabling it causes the value to become null by default: disabled={!metrics}... or does it?
-                  <SliderAndNumber disabled={!metrics} min={0} max={100} step={1} />
-                )}
-              </FormItem>
-            ))}
-            <FormItem label='Total Score' {...layout} >
-              {form.getFieldDecorator('score')(
-                <InputNumber disabled={!metrics} min={0} max={100} />
-              )}
-            </FormItem>
-            <h2>Final Decision</h2>
-            {voting &&
-              <FormItem label='I vote to approve this budget' {...layout}>
-                {form.getFieldDecorator('approved', { valuePropName: 'checked' })(
-                  //  Valueprop is a selector for antd switches, it's in the docs.
-                  <Checkbox size='large' />
-                )}
-              </FormItem>
-            }
-          </div>
+          : <Form onSubmit={this.handleSubmit}>
+            <Row>
+              <Col sm={24} xl={12}>
+                <h2>Your Review</h2>
+                {questions.map(q => (
+                  <FormItem key={q} label={q} {...layout} >
+                    {form.getFieldDecorator(`metrics[${q}]`)(
+                      // FIXME: Disabling it causes the value to become null by default: disabled={!metrics}... or does it?
+                      <SliderAndNumber disabled={!metrics} min={0} max={100} step={1} />
+                    )}
+                  </FormItem>
+                ))}
+                <FormItem label='Total Score' {...layout} >
+                  {form.getFieldDecorator('score')(
+                    <InputNumber disabled={!metrics} min={0} max={100} />
+                  )}
+                </FormItem>
+                <h2>Final Decision</h2>
+                <h4>{voting ? 'Voting is now open. Be forewarned, these decisions are final' : 'Voting is currently closed'}</h4>
+                <FormItem label='I vote to approve this budget' {...layout}>
+                  {form.getFieldDecorator('approved', { valuePropName: 'checked' })(
+                    //  Valueprop is a selector for antd switches, it's in the docs.
+                    <Checkbox disabled={!voting} size='large' />
+                  )}
+                </FormItem>
+                <FormItem>
+                  <Button size='large' type='primary'
+                    htmlType='submit' ghost disabled={!metrics && !voting}
+                    >Update your Review</Button>
+                </FormItem>
+              </Col>
+              <Col sm={24} xl={12}>
+                <h2>Average Scores</h2>
+                <h4>Filter by Commitee Roles</h4>
+                <Switch checked={filter.admin}
+                  unCheckedChildren='Admins' checkedChildren='Admins'
+                  onChange={admin => this.handleFilter({ admin })}
+                />
+                <Switch checked={filter.member}
+                  unCheckedChildren='Members' checkedChildren='Members'
+                  onChange={member => this.handleFilter({ member })}
+                />
+                <Switch checked={filter.spectator}
+                  unCheckedChildren='Ex-Officios' checkedChildren='Ex-Officios'
+                  onChange={spectator => this.handleFilter({ spectator })}
+                />
+              </Col>
+            </Row>
+          </Form>
           }
       </section>
     )
@@ -162,7 +195,9 @@ class Review extends React.Component {
 Review.propTypes = {
   form: PropTypes.object,
   api: PropTypes.object,
+  proposal: PropTypes.string,
   manifest: PropTypes.object,
+  review: PropTypes.object,
   user: PropTypes.object
 }
 export default Review
