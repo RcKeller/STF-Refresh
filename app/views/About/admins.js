631306f548ac6116cb5b7559626510a72fbce68b
Ryan Keller <rykeller@uw.edu>
2017-09-01 11:43:12 -0700

    Refactored About page.

---
+++ app/views/About/admins.js
@@ -0,0 +1,31 @@
+export default [{
+  name: 'Alec Meade',
+  title: 'Chair, former Officer',
+  from: '2016-2018',
+  bio: 'Bio here',
+  duties: 'Duties here'
+}, {
+  name: 'Katie McConville',
+  title: 'Proposal Officer',
+  from: '2017-2018',
+  bio: 'Bio here',
+  duties: 'Duties here'
+}, {
+  name: 'Britt Arram Carlson',
+  title: 'Director, Finance',
+  from: '2017-2018',
+  bio: 'Bio here',
+  duties: 'Duties here'
+}, {
+  name: 'Sara Torres',
+  title: 'Hub Advisor & Manager, Student Life',
+  from: '2017-2018',
+  bio: 'Bio here',
+  duties: 'Duties here'
+}, {
+  name: 'Ryan Keller',
+  title: 'Software Engineer',
+  from: '2017-2018',
+  bio: 'Bio here',
+  duties: 'Duties here'
+}]
---
+++ app/views/About/exOfficios.js
@@ -0,0 +1,13 @@
+export default [{
+  name: 'name',
+  title: 'title',
+  representing: 'ASUW',
+  from: '2017-2018',
+  bio: 'Bio here'
+}, {
+  name: 'name B',
+  title: 'title',
+  representing: 'ASUW',
+  from: '2017-2018',
+  bio: 'Bio here'
+}]
