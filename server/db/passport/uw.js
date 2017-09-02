245f93966454a2c29842a8b0cc1aa5f683a8ed17
Ryan Keller <rykeller@uw.edu>
2017-09-02 10:36:59 -0700

    Recording args in db/passport/uw

--- server/db/passport/uw.js
+++ server/db/passport/uw.js
@@ -1,4 +1,4 @@
 // import User from '../models/user'
 
-export default ({...args}) => console.log('placeholder uw db function received args', args)
+export default (...args) => console.log('args for db/passport/uw.js', args)
 /* eslint-enable no-param-reassign */
