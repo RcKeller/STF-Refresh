d86af0acf0b412ff824a111cc60742838e3482b9
Ryan Keller <rykeller@uw.edu>
2018-02-27 15:28:00 -0800

    Added deserialize, need to include ID when calculating totals too.

--- app/components/Spreadsheet/Spreadsheet.js
+++ app/components/Spreadsheet/Spreadsheet.js
@@ -12,14 +12,34 @@ import ReactDataSheet from 'react-datasheet'
 class FinancialSpreadsheet extends React.Component {
   // headers = ['Name', 'Description', 'Price', 'Tax', 'Quantity', 'TOTAL']
   header = [
-    {value: 'Name', readOnly: true},
-    {value: 'Description / Vendor', readOnly: true},
-    {value: 'Price', readOnly: true},
-    {value: 'Tax', readOnly: true},
-    {value: 'Quantity', readOnly: true},
-    {value: 'TOTAL', readOnly: true}
+    { value: 'Name', readOnly: true },
+    { value: 'Description / Vendor', readOnly: true },
+    { value: 'Price', readOnly: true },
+    { value: 'Tax', readOnly: true },
+    { value: 'Quantity', readOnly: true },
+    { value: 'TOTAL', readOnly: true }
   ]
   footer = [{value: 'Grand Total', readOnly: true, colSpan: 5}, {value: 0, readOnly: true}]
+  serializeManifest = (manifest) => {
+    let data = []
+    for (let item of manifest) {
+      const { _id, name, price, tax, description, quantity } = item || {}
+      //  Create a record in the order of headers
+      const record = [name, description, price, tax, quantity]
+        .map(value => ({ value }))
+      // Push our "summary" cell, containing subtotals and the _id
+      record.push({ _id, value: 0, readOnly: true })
+      data.push(record)
+    }
+    //  Add Header / footer
+    data.unshift(this.header)
+    data.push(this.footer)
+    return data
+  }
+  deserializeManifest = (data) => {
+    console.warn('deserializeManifest - start', data)
+    return data
+  }
   constructor (props) {
     super(props)
     console.error(props)
@@ -30,24 +50,7 @@ class FinancialSpreadsheet extends React.Component {
     Scheme has records that are like [ {value}, {value}, {value}]...
     The final cell is a "summary" cell containing subtotals and item _ids for future ref
     */
-    const serializeManifest = (manifest) => {
-      let data = []
-      for (let item of manifest) {
-        const { _id, name, price, tax, description, quantity } = item || {}
-        //  Create a record in the order of headers
-        const record = [name, description, price, tax, quantity]
-          .map(value => ({ value }))
-        // Push our "summary" cell, containing subtotals and the _id
-        record.push({ _id, value: 0, readOnly: true })
-        data.push(record)
-      }
-      //  Add Header / footer
-      data.unshift(this.header)
-      data.push(this.footer)
-      return data
-    }
-    let transformedData = serializeManifest(data)
-    console.error(transformedData)
+    let transformedData = this.serializeManifest(data)
     this.state = {
       grid: [
         [
@@ -69,7 +72,7 @@ class FinancialSpreadsheet extends React.Component {
       ]
     }
   }
-  generateGrid () {
+  calculateDataWithTotals () {
     const { grid } = this.state
 
     // Make a mutable copy of the grid with values only
@@ -103,68 +106,6 @@ class FinancialSpreadsheet extends React.Component {
     // Update Grand Total
     footer[1] = { value: grandTotal, readOnly: true }
     return grid
-
-    // const taxes = typeof types['tax'] === 'string'
-    // for (let row of rows) {
-    //   const price = row[types['price']]
-    //   const quantity = row[types['quantity']]
-    //   const subtotal = taxes
-    //     ? price * quantity * ((row[types['tax']] / 100) + 1)
-    //     : price * quantity
-    // }
-
-    // const types = {}
-    // for (const [index, cell] of grid[0].entries()) {
-    //   const type = cell.value.toLowerCase()
-    //   types[type] = index
-    // }
-    // console.log('TYPES', types)
-    // for (let row = 1; row < grid.length, row++) {
-    //   let subtotal = 0
-    //   subtotal += (row[types["price"]] * row[types["quantity"]) * (row[types["tax"]])
-    // }
-    // let rows = grid.slice()
-    // let header = rows.shift()
-    // const types = {}
-    // for (const [index, cell] of header.entries()) {
-    //   types[index] = cell.value.toLowerCase()
-    // }
-    // console.log('TYPES', types)
-    // const types = grid[0].map(cell => cell.value)
-    // Typemap becomes a map of enums to cell indexes for grid rows (e.g. row[1] is price, etc)
-
-    // Types - enums for row cells that can be referenced using a cell index
-    // const types = grid[0].map(cell => cell.value)
-
-    // for (let t)
-    // console.warn('TYPES', types)
-
-    // Calculate Subtotals using cell types (Price, Tax, Quantity)
-    // for (let row of grid) {
-    //   let subtotal = 0
-    //   const subtotalCell = row.length - 1
-    //   for (let cell = 0; cell < subtotalCell; cell++) {
-    //
-    //     if types[cell] === 'Tax' {
-    //
-    //     }
-    //     // let value = Number.parseFloat(row[cell].value)
-    //     // subtotal += (Number.isNaN(value) ? 0 : value)
-    //   }
-    //   console.log('SUBTOTAL', subtotal)
-    //   row[subtotalCell] = { value: subtotal, readOnly: true }
-    // }
-    //
-    // // Calculate Grand Total by reducing subtotals
-    // let grandTotal = 0
-    // const totalRowIndex = grid.length - 1
-    // const totalCellIndex = grid[totalRowIndex].length - 1
-    // for (let row of grid) {
-    //   const subtotalCell = row.length - 1
-    //   let value = Number.parseFloat(row[subtotalCell].value)
-    //   grandTotal += (Number.isNaN(value) ? 0 : value)
-    // }
-    // grid[totalRowIndex][totalCellIndex].value = grandTotal
   }
   onCellsChanged = (changes) => {
     const grid = this.state.grid.map(row => [...row])
@@ -174,11 +115,13 @@ class FinancialSpreadsheet extends React.Component {
     this.setState({grid})
   }
   render () {
+    let data = this.calculateDataWithTotals()
+    this.deserializeManifest(data)
     return (
       <div>
         <ReactDataSheet
           // data={this.state.grid}
-          data={this.generateGrid()}
+          data={data}
           valueRenderer={(cell) => (cell.value).toString()}
           onContextMenu={(e, cell, i, j) => cell.readOnly ? e.preventDefault() : null}
           onCellsChanged={changes => {
@@ -266,4 +209,68 @@ constructor (props) {
     ]
   }
 }
+*/
+
+/*
+// const taxes = typeof types['tax'] === 'string'
+// for (let row of rows) {
+//   const price = row[types['price']]
+//   const quantity = row[types['quantity']]
+//   const subtotal = taxes
+//     ? price * quantity * ((row[types['tax']] / 100) + 1)
+//     : price * quantity
+// }
+
+// const types = {}
+// for (const [index, cell] of grid[0].entries()) {
+//   const type = cell.value.toLowerCase()
+//   types[type] = index
+// }
+// console.log('TYPES', types)
+// for (let row = 1; row < grid.length, row++) {
+//   let subtotal = 0
+//   subtotal += (row[types["price"]] * row[types["quantity"]) * (row[types["tax"]])
+// }
+// let rows = grid.slice()
+// let header = rows.shift()
+// const types = {}
+// for (const [index, cell] of header.entries()) {
+//   types[index] = cell.value.toLowerCase()
+// }
+// console.log('TYPES', types)
+// const types = grid[0].map(cell => cell.value)
+// Typemap becomes a map of enums to cell indexes for grid rows (e.g. row[1] is price, etc)
+
+// Types - enums for row cells that can be referenced using a cell index
+// const types = grid[0].map(cell => cell.value)
+
+// for (let t)
+// console.warn('TYPES', types)
+
+// Calculate Subtotals using cell types (Price, Tax, Quantity)
+// for (let row of grid) {
+//   let subtotal = 0
+//   const subtotalCell = row.length - 1
+//   for (let cell = 0; cell < subtotalCell; cell++) {
+//
+//     if types[cell] === 'Tax' {
+//
+//     }
+//     // let value = Number.parseFloat(row[cell].value)
+//     // subtotal += (Number.isNaN(value) ? 0 : value)
+//   }
+//   console.log('SUBTOTAL', subtotal)
+//   row[subtotalCell] = { value: subtotal, readOnly: true }
+// }
+//
+// // Calculate Grand Total by reducing subtotals
+// let grandTotal = 0
+// const totalRowIndex = grid.length - 1
+// const totalCellIndex = grid[totalRowIndex].length - 1
+// for (let row of grid) {
+//   const subtotalCell = row.length - 1
+//   let value = Number.parseFloat(row[subtotalCell].value)
+//   grandTotal += (Number.isNaN(value) ? 0 : value)
+// }
+// grid[totalRowIndex][totalCellIndex].value = grandTotal
 */
